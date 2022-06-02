d9b39c4aaae719ac17a7dd97831ce8626c58eb04
tb1995 <tb1995@gmail.com>
2022-06-02 23:13:25 +0500

    our work page initial

---
+++ src/app/our-work.js
@@ -0,0 +1,32 @@
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+let sections = gsap.utils.toArray(".panel");
+
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".container",
+    pin: true,
+    scrub: 1,
+    snap: 1 / (sections.length - 1),
+    // base vertical scrolling on how wide the container is so it feels more natural.
+    end: "+=3500",
+  }
+});
+
+
+// tl.to(".luxor", {
+//     scrollTrigger: {
+//         trigger: '.luxor',
+//         start: "center center"
+//     },
+//     backgroundColor: "maroon",
+//     delay: 1
+    
+// })
+
