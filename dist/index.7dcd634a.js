4d82111765a0ca0321e1036f4d8bb728dd580f17
tb1995 <tb1995@gmail.com>
2022-07-04 15:16:43 +0500

    navbar

--- dist/index.7dcd634a.js
+++ dist/index.7dcd634a.js
@@ -1,8 +1,17 @@
 let isActive = false;
+let sandwich = document.getElementById("sandwich");
+let fullscreenNav = document.getElementById("fullscreen-nav");
 function toggle(event) {
     event.preventDefault();
+    console.log("testing");
     isActive = !isActive;
-    isActive;
+    if (isActive) {
+        sandwich.classList.add("is-active");
+        fullscreenNav.classList.add("fullscreen-is-active");
+    } else {
+        sandwich.classList.remove("is-active");
+        fullscreenNav.classList.remove("fullscreen-is-active");
+    }
 }
 
 //# sourceMappingURL=index.7dcd634a.js.map
--- src/app/navbar.js
+++ src/app/navbar.js
@@ -1,12 +1,22 @@
 
 let isActive = false;
+let sandwich = document.getElementById('sandwich')
+let fullscreenNav = document.getElementById('fullscreen-nav')
+
 
 function toggle(event) {
     event.preventDefault();
+    console.log("testing")
 
     isActive = !isActive
     if(isActive)
  {
-     
- }}
+    sandwich.classList.add('is-active')
+    fullscreenNav.classList.add('fullscreen-is-active')
+ } else {
+     sandwich.classList.remove('is-active')
+     fullscreenNav.classList.remove('fullscreen-is-active')
+ }
+
+}
 
